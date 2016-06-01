77ebf45f95a07785095d21dc61d436e60b16d0d9
youxin.zhuo <zhuoyouxin@zhuoyouxindeMac-mini.local>
2016-06-01 14:42:49 +0800

    update file

--- ZYXQuiz/ZYXQuiz/ViewController.swift
+++ ZYXQuiz/ZYXQuiz/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var qustionLabel: UILabel!
+    @IBOutlet weak var currentQuestionLabel: UILabel!
+    @IBOutlet weak var nextQuestionLabel: UILabel!
     
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     
     let questions: [String] = ["From what is cognac made?", "What is 7+77", "What is the capital of Vermont"]
     
@@ -29,8 +33,10 @@ class ViewController: UIViewController {
         }
         
         let question: String = questions[currentQustionIndex]
-        qustionLabel.text = question
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
+        
+        animateLabelTransitions()
     }
     
     @IBAction func showAnswer(sender: AnyObject)
@@ -42,7 +48,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        qustionLabel.text = questions[currentQustionIndex]
+        currentQuestionLabel.text = questions[currentQustionIndex]
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        nextQuestionLabel.alpha = 0
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,6 +63,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func animateLabelTransitions()
+    {
+        view.layoutIfNeeded()
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
+        UIView.animateWithDuration(0.5, delay: 0, options: [.CurveLinear], animations: {
+            self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1
+            self.view.layoutIfNeeded()
+            }) { (_) in
+                swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+                
+                self.updateOffScreenLabel()
+        }
+    }
+    
+    func updateOffScreenLabel()
+    {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
+        
+    }
 }
 

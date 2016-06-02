fcace7f40618cd6d6b63a8b2197a4d660afca4ac
zhuodream <278941654@qq.com>
2016-06-03 01:05:17 +0800

    add new file

--- ZYXHomepwner/ZYXHomepwner/ImageStore.swift
+++ ZYXHomepwner/ZYXHomepwner/ImageStore.swift
@@ -15,15 +15,55 @@ class ImageStore
     func setImage(image: UIImage, forKey key:String)
     {
         cache.setObject(image, forKey: key)
+        
+        let imageURL = imageURLForKey(key)
+        
+        if let data = UIImageJPEGRepresentation(image, 0.5)
+        {
+            data.writeToURL(imageURL, atomically: true)
+        }
+        
     }
     
     func imageForKey(key: String) -> UIImage?
     {
-        return cache.objectForKey(key) as? UIImage
+        if let existingImage = cache.objectForKey(key) as? UIImage
+        {
+            return existingImage
+        }
+        
+        let imageURL = imageURLForKey(key)
+        guard let imageFromDisk = UIImage(contentsOfFile: imageURL.path!) else
+        {
+            return nil
+        }
+        
+        cache.setObject(imageFromDisk, forKey: key)
+        
+        return imageFromDisk
     }
     
     func deleteImageForKey(key: String)
     {
         cache.removeObjectForKey(key)
+        
+        let imageURL = imageURLForKey(key)
+        do {
+            try NSFileManager.defaultManager().removeItemAtURL(imageURL)
+        }
+        catch let deleteError {
+            print("Error removing the image from disk: \(deleteError)")
+        }
+        
     }
+    
+    func imageURLForKey(key: String) -> NSURL
+    {
+        let documentDirectories = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let documentDirectory = documentDirectories.first!
+        
+        return documentDirectory.URLByAppendingPathComponent(key)
+    }
+    
+    
 }
